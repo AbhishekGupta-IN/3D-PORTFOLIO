de47f36cef4383458580c40682d5bab9b6606bd5
Abhishek Gupta <abhishekgupt.in@gmail.com>
2023-06-10 19:44:02 +0530

    implemented work and feedback section

--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -13,7 +13,7 @@ function HOC() {
             initial='hidden'
             whileInView='show'
             viewport={{ once: true, amount: 0.25 }}
-            className={`${styles.padding} max-w-7xl max-auto relative z-0`}
+            className={`${styles.padding} mx-auto max-w-7xl max-auto relative z-0`}
         >
             <span className='hash-span' id={idName}> &nbsp; </span>
             <Component />
