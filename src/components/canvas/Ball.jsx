97e1edd9c6097cd7a70719c0e9472c493d6b6573
Abhishek Gupta <abhishekgupt.in@gmail.com>
2023-06-23 20:07:46 +0530

    testing some changes

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -8,23 +8,17 @@ const Ball = (props) => {
   const [decal] = useTexture([props.imgUrl]);
   return (
     <Float
-      speed={1.75}
-      rotationIntensity={1}
       floatIntensity={2}
     >
       <ambientLight intensity={0.25} />
       <directionalLight position={[0, 0, 0.05]} />
-      <mesh castShadow receiveShadow scale={2.75}>
+      <mesh scale={2.75}>
         <icosahedronGeometry args={[1, 1]} />
         <meshStandardMaterial
-          color="#fff8eb"
-          polygonOffset
-          polygonOffsetFactor={-5}
           flatShading
         />
         <Decal
           position={[0, 0, 1]}
-          rotation={[2 * Math.PI, 0, 6.25 ]}
           map={decal}
         />
       </mesh>
@@ -34,17 +28,13 @@ const Ball = (props) => {
 
 const BallCanvas = ({ icon }) => {
   return (
-    <Canvas
-      gl={{ preserveDrawingBuffer: true }}
-    >
+    <Canvas>
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
           enableZoom={false}
         />
         <Ball imgUrl={icon} />
       </Suspense>
-
-      <Preload all />
     </Canvas>
   )
 }
